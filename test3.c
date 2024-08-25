d9bd40aed50d070a269d617c6737d840098061ba
atbicer <atbicer@student.s19.be>
2024-08-25 16:30:41 +0200

    minilibx tests

--- test3.c
+++ test3.c
@@ -1,4 +1,5 @@
 #include "minilibx/mlx.h"
+#include <stdlib.h>
 
 #define WIDTH 500
 #define HEIGHT 500
@@ -13,7 +14,8 @@ int	main(void)
 	for (int y = HEIGHT * 0.1; y < HEIGHT * 0.9; ++y)
 	{
 		for (int x = WIDTH * 0.1; x < WIDTH * 0.9; ++x)
-			mlx_pixel_put(mlx, mlx_window,x, y, 0xff0000);
+			mlx_pixel_put(mlx, mlx_window,x, y, rand() % 0x1000000);
 	}
+	mlx_string_put(mlx, mlx_window, WIDTH * 0.8, HEIGHT * 0.95, rand() % 0x1000000, "ATBICER");
 	mlx_loop(mlx);
 }
